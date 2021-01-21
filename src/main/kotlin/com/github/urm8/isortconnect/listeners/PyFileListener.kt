ebfb1304c925c3d16f0650aad3f957426df4bedc
Levon Saldamli <levon.saldamli@amramedical.com>
2021-01-21 22:59:39 +0100

    Sort all files instead of just the first and code style

--- src/main/kotlin/com/github/urm8/isortconnect/listeners/PyFileListener.kt
+++ src/main/kotlin/com/github/urm8/isortconnect/listeners/PyFileListener.kt
@@ -3,47 +3,61 @@ package com.github.urm8.isortconnect.listeners
 import com.github.urm8.isortconnect.service.SorterService
 import com.github.urm8.isortconnect.settings.IsortConnectService
 import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectLocator
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.vfs.AsyncFileListener
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import com.jetbrains.python.PythonFileType
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
+
+data class PyFile(val file: VirtualFile, val project: Project)
 
 class PyFileListener : AsyncFileListener {
 
     override fun prepareChange(events: MutableList<out VFileEvent>): AsyncFileListener.ChangeApplier? {
         val locator = ProjectLocator.getInstance()
+        val filesToUpdate = mutableSetOf<PyFile?>()
         for (
             file in events
                 .mapNotNull { event -> event.file }
                 .filter { file -> file.fileType == PythonFileType.INSTANCE }
         ) {
-            return locator.guessProjectForFile(file)?.run {
-                return if (ProjectRootManager
-                    .getInstance(this)
-                    .fileIndex.run {
-                        this.isInSource(file) ||
-                            this.isInContent(file) ||
-                            this.isInSourceContent(file)
-                    } &&
-                    this.service<IsortConnectService>().state.triggerOnSave
+            val pyFile = locator.guessProjectForFile(file)?.run {
+                if (isSourceFileAndTriggerOnSave(file)
                 ) {
-                    PyFileApplier(file, this)
+                    PyFile(file, this)
                 } else {
                     null
                 }
             }
+            filesToUpdate.add(pyFile)
         }
-        return null
+        return PyFileApplier(filesToUpdate.filterNotNull())
     }
 
-    class PyFileApplier(private val vf: VirtualFile, private val proj: Project) : AsyncFileListener.ChangeApplier {
+    private fun @Nullable Project.isSourceFileAndTriggerOnSave(file: @Nullable VirtualFile) =
+        ProjectRootManager
+            .getInstance(this)
+            .fileIndex.run {
+                inSourceContent(file) && FileDocumentManager.getInstance().isFileModified(file)
+            } && this.service<IsortConnectService>().state.triggerOnSave
+
+    private fun @NotNull ProjectFileIndex.inSourceContent(file: @Nullable VirtualFile) =
+        this.isInSource(file) || this.isInContent(file) || this.isInSourceContent(file) ||
+            this.isInTestSourceContent(file)
+
+    class PyFileApplier(private val filesToUpdate: Collection<PyFile>) : AsyncFileListener.ChangeApplier {
         override fun afterVfsChange() {
             super.afterVfsChange()
-            val sorter = proj.service<SorterService>()
-            sorter.sort(vf)
+            filesToUpdate.forEach { pyFile ->
+                val sorter = pyFile.project.service<SorterService>()
+                sorter.sort(pyFile.file)
+            }
         }
     }
 
